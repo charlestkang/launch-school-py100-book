ffb788b94ba5f22e67a5f85ded2f1fc579d22991
Charles Kang <204531806+charlestkang@users.noreply.github.com>
2026-08-26 17:43:21 +0900

    Add edited greeter.py pursuant to instructions

--- 04_input_output/greeter.py
+++ 04_input_output/greeter.py
@@ -1,2 +1,3 @@
-NAME = input('What is your name? ')
-print(f'Hello, {NAME}!')
+first_name = input('What is your first name? ')
+last_name = input('What is your last name? ')
+print(f'Hello, {first_name} {last_name}!')
