ef4b5f9535a3439e1f2fcd59e246bd3dd20f4e87
Charles Kang <204531806+charlestkang@users.noreply.github.com>
2026-09-01 16:01:15 +0900

    Add exercises for 11_more_stuff

---
+++ 11_more_stuff/exercise_05.py
@@ -0,0 +1,30 @@
+# EXERCISE 5
+# On reflection, we've decided that we don't want to rely on using a global variable in the code 
+# we wrote in the previous example. To fix this, we're going to nest the code from the previous 
+# example into an outer function:
+
+# Copy Code
+def all_actions():
+    counter = 0
+
+    def increment_counter():
+        # global counter # Preserved initial bug
+        nonlocal counter
+        counter += 1
+
+    print(counter)                # 0
+
+    increment_counter()
+    print(counter)                # 1
+
+    increment_counter()
+    print(counter)                # 2
+
+    counter = 100
+    increment_counter()
+    print(counter)                # 101
+
+all_actions()
+# There's a bug in this code. Identify the bug, and fix it.
+
+# ANSWER: Bug is that line 5 declares global counter when it should be nonlocal
